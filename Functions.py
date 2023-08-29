4772c354a354d0462d408cd7b872f0e8d844e90e
NirdeshSrivastav <srivastavnirdesh4@gmail.com>
2023-08-29 20:16:06 +0530

    update Functions .py project

---
+++ Functions.py
@@ -0,0 +1,17 @@
+# Function definition
+def gmen(a, b):
+    mean = (a * b) / (a + b)
+    print(mean)
+
+
+def great(a, b):
+    if a > b:
+        print("A is greater")
+    else:
+        print("B is greater")
+
+
+# driver code
+a = 9
+b = 8
+gmen(a, b)  # Function call
