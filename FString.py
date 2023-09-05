eac7d5c78d002dbdb9514d174961ea6967f0e2f5
NirdeshSrivastav <srivastavnirdesh4@gmail.com>
2023-09-05 07:12:38 +0530

    update FString.py

--- FString.py
+++ FString.py
@@ -1,25 +1,24 @@
 # f string is used to format strings
-#-----------------OLD Approach----------------
-letter = "Hello my namee is {} and i am from {}"
+# ----------------- OLD Approach ----------------
+letter = "Hello my name is {} and i am from {}"
 country = "India"
 name = "Nirdesh"
 print(letter.format(name, country))
-letter = "Hello my namee is {1} and i am from {0}"
-print(letter.format(country, name ))
+letter = "Hello my name is {1} and i am from {0}"
+print(letter.format(country, name))
 
-# -------------------MODERN APPROACH----------------
-print(f"Hello my namee is {name} and i am from {country}")  # This is called fstrings
+# ------------------- MODERN APPROACH ----------------
+print(f"Hello my name is {name} and i am from {country}")  # This is called fstrings
 
 price = 49.099999
-txt = "For only {price: .2f} dollors"
-print(txt.format(price = 49.099999))
+txt = "For only {price: .2f} dollars"
+print(txt.format(price=49.099999))
 # OR with f string
-txt = f"For only {price: .2f} dollors"
+txt = f"For only {price: .2f} dollars"
 print(txt)
 
 # we can find a string
 print(f"{2 * 30}")
 
 # if we want to print this as it is
-print(f"Hello my namee is {{name}} and i am from {{country}}")  # This is called fstrings
-
+print(f"Hello my name is {{name}} and i am from {{country}}")  # This is called fstrings
